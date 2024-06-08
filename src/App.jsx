aa44d4bcf3852bd357838487b4a87c9cb2a30082
Irae Veras <irae.online@gmail.com>
2024-06-08 14:49:33 -0300

    implementand a lógica da busca dinânmica

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
 import { useState } from "react";
 import { FiSearch } from "react-icons/fi";
+import api from "./services/api";
 import "./App.css";
 function App() {
 
   const [input, setInput] = useState("")
+  const [cep, setCep] = useState({})
 
-  function hendleSearch() {
+  async function hendleSearch() {
     if (input === "") {
       alert("digite um cep.")
       return
     }
 
     try {
-      
+      const response = await api.get(`${input}/json`);
+
+      setCep(response.data)
+      setInput("")
+
     } catch (error) {
-      
+
+      alert("Erro ao buscar cep. ");
+      setInput("")
     }
   }
 
@@ -36,14 +44,17 @@ function App() {
           <FiSearch size={25} color="#fff" />
         </button>
       </div>
-
-      <main className="main">
-        <h2>CEP: 54420200</h2>
-        <span>Rua:</span>
-        <span>Complemento:</span>
-        <span>Vila ROsa</span>
-        <span>Campo Grande - MS</span>
-      </main>
+      {Object.keys(cep).length > 0 && (
+        <main className="main">
+          
+          <h2>CEP: {cep.cep}</h2>
+          <span>{cep.logradouro}</span>
+          <span>{cep.complemento === "" ? "Sem comlpemento" : `Complemento: ${cep.complemento}`}</span>
+          <span>{cep.bairro}</span>
+          <span>{cep.localidade} - {cep.uf}</span>
+        </main>
+      )}
+      
     </div>
   )
 }
