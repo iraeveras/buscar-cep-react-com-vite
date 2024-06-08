1f775ba32bcaede03178c6af99ae096bddee4646
Irae Veras <irae.online@gmail.com>
2024-06-08 15:29:10 -0300

    ajustes finais

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
 
   return (
     <div className="container">
-      <h1 className="title">Buscador de CEP:</h1>
+      <h1 className="title">Buscador de CEP</h1>
       <div className="container-input">
         <input 
         type="text" 
